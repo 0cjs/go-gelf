90f8d2cc213a91ad4300c755aec3fcd25564d6ca
MiLk <hello@emilienkenler.com>
2017-07-29 09:28:31 +0900

    delimited writer: test more extensively

--- gelf/gelf_test.go
+++ gelf/gelf_test.go
@@ -75,16 +75,58 @@ func TestDelimitedWriter(t *testing.T) {
 	assert.Equal(t, "ab-c-def-", buf.String())
 }
 
-type failingWriter struct{}
-
-func (w failingWriter) Write(b []byte) (int, error) {
-	return 1337, errors.New("failingWriter")
+type configurableWriter struct {
+	fn func(b []byte) (int, error)
 }
+
+func (w *configurableWriter) Write(b []byte) (int, error) {
+	return w.fn(b)
+}
+
 func TestDelimitedWriter_Write(t *testing.T) {
-	dw := newDelimitedWriter(failingWriter{}, 'x')
+	failingWriter := configurableWriter{func(b []byte) (int, error) {
+		return 1337, errors.New("failingWriter")
+	}}
+	dw := newDelimitedWriter(&failingWriter, 'x')
 	n, err := dw.Write([]byte("ab"))
 	assert.EqualError(t, err, "failingWriter")
-	assert.Equal(t, n, 1337)
+	assert.Equal(t, 1337, n)
+}
+
+func TestDelimitedWriter_Write2(t *testing.T) {
+	incompleteWriter := configurableWriter{func(b []byte) (int, error) {
+		return len(b) / 2, nil
+	}}
+	dw := newDelimitedWriter(&incompleteWriter, 'x')
+	assert.Panics(t, func() {
+		dw.Write([]byte("ab"))
+	})
+}
+
+func TestDelimitedWriter_Write3(t *testing.T) {
+	failingDelimiterWriter := configurableWriter{func(b []byte) (int, error) {
+		if len(b) == 1 {
+			return 0, errors.New("failingDelimiterWriter")
+		}
+		return len(b), nil
+	}}
+	dw := newDelimitedWriter(&failingDelimiterWriter, 'x')
+	n, err := dw.Write([]byte("ab"))
+	assert.EqualError(t, err, "failingDelimiterWriter")
+	assert.Equal(t, 2, n)
+}
+
+func TestDelimitedWriter_Write4(t *testing.T) {
+	incompleteWriter := configurableWriter{func(b []byte) (int, error) {
+		if len(b) == 1 {
+			return 0, nil
+		}
+		return len(b), nil
+	}}
+	dw := newDelimitedWriter(&incompleteWriter, 'x')
+	assert.Panics(t, func() {
+		dw.Write([]byte("ab"))
+	})
 }
 
 type mockWriteResetter struct {
