87f1dfd73f972e9b0b472713b9fa85c790835a46
MiLk <hello@emilienkenler.com>
2017-07-30 10:12:50 +0900

    stream: move stream tests to a dedicated test file

--- gelf/gelf_test.go
+++ gelf/gelf_test.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"compress/gzip"
 	"compress/zlib"
-	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -49,77 +48,4 @@ func TestWriter_Write2(t *testing.T) {
 	length, err := g.Write([]byte(data))
 	assert.Nil(t, err)
 	assert.Equal(t, len(data), length)
-}
-
-func TestDelimitedWriter(t *testing.T) {
-	buf := new(bytes.Buffer)
-	dw := newDelimitedWriter(buf, '-')
-
-	n, err := dw.Write([]byte("ab"))
-	assert.Nil(t, err)
-	assert.EqualValues(t, 2, n)
-
-	n, err = dw.Write([]byte("c"))
-	assert.Nil(t, err)
-	assert.EqualValues(t, 1, n)
-
-	n, err = dw.Write([]byte("def"))
-	assert.Nil(t, err)
-	assert.EqualValues(t, 3, n)
-
-	assert.Equal(t, "ab-c-def-", buf.String())
-}
-
-type configurableWriter struct {
-	fn func(b []byte) (int, error)
-}
-
-func (w *configurableWriter) Write(b []byte) (int, error) {
-	return w.fn(b)
-}
-
-func TestDelimitedWriter_Write(t *testing.T) {
-	failingWriter := configurableWriter{func(b []byte) (int, error) {
-		return 1337, errors.New("failingWriter")
-	}}
-	dw := newDelimitedWriter(&failingWriter, 'x')
-	n, err := dw.Write([]byte("ab"))
-	assert.EqualError(t, err, "failingWriter")
-	assert.Equal(t, 1337, n)
-}
-
-func TestDelimitedWriter_Write2(t *testing.T) {
-	incompleteWriter := configurableWriter{func(b []byte) (int, error) {
-		return len(b) / 2, nil
-	}}
-	dw := newDelimitedWriter(&incompleteWriter, 'x')
-	assert.Panics(t, func() {
-		dw.Write([]byte("ab"))
-	})
-}
-
-func TestDelimitedWriter_Write3(t *testing.T) {
-	failingDelimiterWriter := configurableWriter{func(b []byte) (int, error) {
-		if len(b) == 1 {
-			return 0, errors.New("failingDelimiterWriter")
-		}
-		return len(b), nil
-	}}
-	dw := newDelimitedWriter(&failingDelimiterWriter, 'x')
-	n, err := dw.Write([]byte("ab"))
-	assert.EqualError(t, err, "failingDelimiterWriter")
-	assert.Equal(t, 2, n)
-}
-
-func TestDelimitedWriter_Write4(t *testing.T) {
-	incompleteWriter := configurableWriter{func(b []byte) (int, error) {
-		if len(b) == 1 {
-			return 0, nil
-		}
-		return len(b), nil
-	}}
-	dw := newDelimitedWriter(&incompleteWriter, 'x')
-	assert.Panics(t, func() {
-		dw.Write([]byte("ab"))
-	})
 }
